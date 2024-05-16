1a8535d7c8168c46948afc8f33febe2404102d58
Justine-Lance-Pabalan <23-07377@g.batstate-u.edu.ph>
2024-05-17 05:55:08 +0800

    working on it

--- main.py
+++ main.py
@@ -3,9 +3,12 @@ from utils.user_manager import UserManager
 from utils.user import User
 from utils.score import Score
 
+UM_i = UserManager()
+U_i = User()
+
 def main():
     while True:
-        print(f"Welcome, {self.username}")
+        print(f"Welcome to the Dice Roll Game!")
         print("1. Register")
         print("2. Login")
         print("3. Exit")
@@ -13,11 +16,13 @@ def main():
         choice = input("Enter the number of your choice: ")
 
         if choice == "1":
-            def register()
+            UM_i.register()
         elif choice == "2":
-            def login()
+            UM_i.login()
         elif choice == "3":
             break
         else:
             print("Invalid choice. Please try again.")
             return
+
+main()
--- utils/user_manager.py
+++ utils/user_manager.py
@@ -1,33 +1,43 @@
 from .user import User
 
 class UserManager:
+	Users = {}
+
 	def load_users():
 		pass
 
 	def save_users():
 		pass
 
-	def validate_username():
-		pass
+	def validate_username(self, username):
+		if len(username) <4:
+			print("Username must be at least 4 characters long.")
+			return False
+		else:
+			return True
+	def validate_password(self, password):
+		if len(password) <8:
+			print("Password must be at least 8 characters long.")
+			return False
+		else:
+			return True
 
-	def validate_password():
-		pass
-
-	def register():
+	def register(self):
 		username = input(str("Enter username (at least 4 characters), or leave blank to cancel: "))
 		if username == "":
 			return
-		elif len(username) <4:
-			print("Username must be at least 4 characters long.")
-			return
-		password = input(str("Enter password (at least 8 characters), or leave blank to cancel: "))
-		if password == "":
-			return
-		elif len(password) <8:
-			print("Password must be at least 8 characters long.")
-			return
 		else:
-			retur
-		
+			self.validate_username(username)
+			password = input(str("Enter password (at least 8 characters), or leave blank to cancel: "))
+			if password == "":
+				return
+			else: 
+				if username in self.Users:
+					print("Username already exists")
+				else:
+					self.validate_password(password)
+					print("Registration successful")
+					self.Users[username].append(username, password)
+					return
 	def login():
 		pass
