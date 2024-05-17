239c4fabdc4306a22a701aff870355ba50c1eacc
Justine-Lance-Pabalan <23-07377@g.batstate-u.edu.ph>
2024-05-17 11:19:50 +0800

    almost done

--- utils/score.py
+++ utils/score.py
@@ -1,5 +1,5 @@
 class Score:
-	def __init__(self, username, points, stages_won, date):
+	def _init_(self, username, points, stages_won, date):
 		self.username = username
 		self.points = points
 		self.stages_won = stages_won
--- utils/user.py
+++ utils/user.py
@@ -1,4 +1,4 @@
 class User:
-	def __init__(self, username, password):
+	def _init_(self, username, password):
 		self.username = username
 		self.password = password
